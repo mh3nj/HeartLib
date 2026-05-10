1ddc0c9da541975766e92f67ea9c8f9bfba5c11c
Mohsen Jafari <anoushzadparsi@gmail.com>
2026-05-10 15:02:59 +0000

    Create __init__.py

---
+++ gui/__init__.py
@@ -0,0 +1,6 @@
+# heartlib/gui/__init__.py
+from .quick_actions import QuickActionsWidget
+from .search_results import SearchResultsWidget
+from .patron_spotlight import PatronSpotlightWidget
+from .circulation_feed import CirculationFeedWidget
+from .details_panel import DetailsPanelWidget
